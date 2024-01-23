926506a96832b285e283fa4b3046012dc9b179e9
Sirot2021 <fabian.sturm3@web.de>
2024-01-23 16:58:17 +0100

    Applikation an unser BPM-Diagramm angebunden

--- bpm-case-study-travel-insurance/Prozessanwendung (Demo)/src/main/java/de/viadee/bpm/camunda/travelinsuranceprocessapp/TravelInsuranceProcessAppApplication.java
+++ bpm-case-study-travel-insurance/Prozessanwendung (Demo)/src/main/java/de/viadee/bpm/camunda/travelinsuranceprocessapp/TravelInsuranceProcessAppApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @Deployment(resources = "classpath*:/process/**/*.bpmn")
 public class TravelInsuranceProcessAppApplication {
-	public static final String TRAVEL_INSURANCE_PROCESS_ID = "TravelInsuranceProcess";
+	public static final String TRAVEL_INSURANCE_PROCESS_ID = "TravelInsuranceProcessGruppe3";
 
 	public static void main(String[] args) {
 		SpringApplication.run(TravelInsuranceProcessAppApplication.class, args);
