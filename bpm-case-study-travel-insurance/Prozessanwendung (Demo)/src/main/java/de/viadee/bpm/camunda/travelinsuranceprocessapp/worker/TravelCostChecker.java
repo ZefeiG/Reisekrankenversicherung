858a52c77526d15c515b3b4b208598da6a1c67a2
ZEFEI GAO <84627771+ZefeiG@users.noreply.github.com>
2024-01-27 02:05:42 +0100

    Update TravelCostChecker.java
    add row 12;
    
    row 16 delete boolean

--- bpm-case-study-travel-insurance/Prozessanwendung (Demo)/src/main/java/de/viadee/bpm/camunda/travelinsuranceprocessapp/worker/TravelCostChecker.java
+++ bpm-case-study-travel-insurance/Prozessanwendung (Demo)/src/main/java/de/viadee/bpm/camunda/travelinsuranceprocessapp/worker/TravelCostChecker.java
@@ -8,9 +8,12 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class TravelCostChecker {
+
+static boolean travelCostIsPositive;
+    
     @JobWorker(type = "check-travel-cost", fetchVariables = {"travelCost"})
     public void checkTravelCost(final JobClient client, final ActivatedJob job, @Variable int travelCost) {
-        boolean travelCostIsPositive = travelCost > 0;
+        travelCostIsPositive = travelCost > 0;
         client.newCompleteCommand(job.getKey())
                 .variable("travelCostIsValid", travelCostIsPositive)
                 .send()
