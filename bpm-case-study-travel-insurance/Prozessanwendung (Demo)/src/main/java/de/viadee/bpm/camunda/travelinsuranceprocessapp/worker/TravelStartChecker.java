def45bb260406c9d639cb4fda740917705e5acd6
ZEFEI GAO <84627771+ZefeiG@users.noreply.github.com>
2024-01-27 02:07:53 +0100

    Update TravelStartChecker.java
    add row 13
    row 17 delete boolean

--- bpm-case-study-travel-insurance/Prozessanwendung (Demo)/src/main/java/de/viadee/bpm/camunda/travelinsuranceprocessapp/worker/TravelStartChecker.java
+++ bpm-case-study-travel-insurance/Prozessanwendung (Demo)/src/main/java/de/viadee/bpm/camunda/travelinsuranceprocessapp/worker/TravelStartChecker.java
@@ -9,10 +9,12 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDate;
 @Component
 public class TravelStartChecker {
-
+    
+    static boolean travelStartIsInFuture;
+    
     @JobWorker(type = "check-travel-start", fetchVariables = {"travelStart"})
     public void checkPlaceOfResidence(final JobClient client, final ActivatedJob job, @Variable String travelStart) {
-        boolean travelStartIsInFuture = isInFuture(LocalDate.parse(travelStart));
+        travelStartIsInFuture = isInFuture(LocalDate.parse(travelStart));
         client.newCompleteCommand(job.getKey())
                 .variable("travelStartIsValid", travelStartIsInFuture)
                 .send()
