cb6e91bc640978e9716ddb87376b5428046c92a7
Sirot2021 <fabian.sturm3@web.de>
2024-01-27 12:54:06 +0100

    fixed everything

--- bpm-case-study-travel-insurance/Prozessanwendung (Demo)/src/main/java/de/viadee/bpm/camunda/travelinsuranceprocessapp/worker/AmountChecker.java
+++ bpm-case-study-travel-insurance/Prozessanwendung (Demo)/src/main/java/de/viadee/bpm/camunda/travelinsuranceprocessapp/worker/AmountChecker.java
@@ -4,24 +4,15 @@ import io.camunda.zeebe.client.api.response.ActivatedJob;
 import io.camunda.zeebe.client.api.worker.JobClient;
 import io.camunda.zeebe.spring.client.annotation.JobWorker;
 import io.camunda.zeebe.spring.client.annotation.Variable;
-import org.json.*;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestClientException;
 
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.util.Base64;
-
-import static de.viadee.bpm.camunda.travelinsuranceprocessapp.service.Authentication.getBasicAuthenticationHeader;
 @Component
 public class AmountChecker {
 
 static boolean amountLessThan7;
 
     @JobWorker(type = "check-amount")
-    public void checkAmount(final JobClient client, final ActivatedJob job, @Variable int numberInsuredPartners) throws Exception {
+    public void checkAmount(final JobClient client, final ActivatedJob job, @Variable int numberInsuredPartners){
 
         amountLessThan7 = numberInsuredPartners < 7;
 
